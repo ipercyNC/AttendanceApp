1d4d24c03df6981c2c7a08ef20ede0471d9605fc
Ian Percy <ianpercyor@gmail.com>
2020-12-11 15:32:41 -0800

    login debugging heroku

--- backend/controllers/login.js
+++ backend/controllers/login.js
@@ -5,6 +5,7 @@ const User = require('../models/user');
 
 loginRouter.post('/', async (request, response) => {
 	const body = request.body.userObject;
+	console.log('body backend', body);
 	const user = await User.findOne({ username: body.username });
 	const passwordCorrect = user === null
 		? false
